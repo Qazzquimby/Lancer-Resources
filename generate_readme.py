58754863506c6eacdeddcbe76fce94e8d399ccda
Toren Darby <torendarby@gmail.com>
2022-09-03 13:43:29 -0700

    refactor: split into modules

---
+++ generate_readme.py
@@ -0,0 +1,176 @@
+from collections import defaultdict
+from pathlib import Path
+
+from objects import CORPS, SOURCES, AUTHORS, Mech, MECHS
+from resources import Image, MechResource, RESOURCE_GROUPS, _is_image_group
+
+
+def _generate_mech_readme_from_group_name_to_content(
+    mech: Mech, groups_to_content: dict[str, list[MechResource]]
+) -> str:
+    content_groups = {}
+    image_groups = {}
+    for name, content in groups_to_content.items():
+        if _is_image_group(content):
+            image_groups[name] = content
+        else:
+            content_groups[name] = content
+
+    lines = [
+        f"### {mech.name.capitalize().replace('_', ' ')}\n",
+        f"Corp: {mech.corp}\n",
+        f"Source: {mech.source}\n",
+        f"Author: {mech.author}\n",
+    ]
+    if mech.summary:
+        for line in mech.summary:
+            lines.append(f"- {line}\n")
+
+    if mech.image_path:
+        if not isinstance(mech.image_path, list):
+            mech.image_path = [mech.image_path]
+        for image_path in mech.image_path:
+            lines.append(
+                f'<img src="{image_path}" width="500"/>\n',
+            )
+
+    for name, content in content_groups.items():
+        if not name or not content:
+            continue  # This assumes non-image resources always have a name
+        lines.append(f"#### {name}")
+        if not isinstance(content, list):
+            content = [content]
+        for item in content:
+            lines.append(f"- {item}")
+
+    if image_groups:
+        lines.append("<details>")
+        lines.append("<summary>Images</summary>\n")
+        named_image_groups = {
+            name: content for name, content in image_groups.items() if name
+        }
+        unnamed_image_groups = {
+            name: content for name, content in image_groups.items() if not name
+        }
+        for name, content in named_image_groups.items():
+            lines.append(f"##### {name}")
+            if not isinstance(content, list):
+                content = [content]
+            for item in content:
+                assert isinstance(item, Image)
+                lines.append(item.get_readme())
+
+        if unnamed_image_groups:
+            lines.append(f"##### miscellaneous")
+            for name, content in unnamed_image_groups.items():
+                if name:
+                    continue
+                if not isinstance(content, list):
+                    content = [content]
+                for item in content:
+                    assert isinstance(item, Image)
+                    lines.append(item.get_readme())
+
+    lines.append("</details>")
+
+    text = "\n".join(lines)
+    return text
+
+
+def generate_mech_readme(mech: Mech) -> str:
+    group_names_to_content: dict[str, list[MechResource]] = defaultdict(list)
+    for resource_group in RESOURCE_GROUPS:
+        contents = resource_group.resources.get(mech, [])
+        if not isinstance(contents, list):
+            contents = [contents]
+        group_names_to_content[resource_group.name] += contents
+
+    mech_readme = _generate_mech_readme_from_group_name_to_content(
+        mech, group_names_to_content
+    )
+    return mech_readme
+
+
+def get_mech_toc_line(mech: Mech) -> str:
+    header_link = mech.name.replace(" ", "-").replace("'", "")
+    return f"- [{mech.name.capitalize()}](#{header_link})"
+
+
+def generate_all_mechs_toc() -> str:
+    toc_lines = ["<details>\n", "<summary>All Mechs</summary>\n"]
+    for mech in MECHS:
+        toc_lines.append(get_mech_toc_line(mech))
+    toc_lines.append("</details>")
+    toc = "\n".join(toc_lines)
+    return toc
+
+
+def generate_toc_by_corp() -> str:
+    toc_lines = ["<details>\n", "<summary>Mechs by Corp</summary>\n"]
+    for corp in CORPS:
+        toc_lines.append("<details>")
+        toc_lines.append(f"<summary>{corp}</summary>\n")
+        for mech in MECHS:
+            if mech.corp == corp:
+                toc_lines.append(get_mech_toc_line(mech))
+        toc_lines.append("</details>")
+    toc_lines.append("</details>")
+    toc = "\n".join(toc_lines)
+    return toc
+
+
+def generate_toc_by_source() -> str:
+    toc_lines = ["<details>\n", "<summary>Mechs by Source</summary>\n"]
+    for source in SOURCES:
+        toc_lines.append("<details>")
+        toc_lines.append(f"<summary>{source}</summary>\n")
+        for mech in MECHS:
+            if mech.source == source:
+                toc_lines.append(get_mech_toc_line(mech))
+        toc_lines.append("</details>")
+    toc_lines.append("</details>")
+    toc = "\n".join(toc_lines)
+    return toc
+
+
+def generate_toc_by_author() -> str:
+    toc_lines = ["<details>\n", "<summary>Mechs by Author</summary>\n"]
+    for author in AUTHORS:
+        toc_lines.append("<details>")
+        toc_lines.append(f"<summary>{author}</summary>\n")
+        for mech in MECHS:
+            if mech.author == author:
+                toc_lines.append(get_mech_toc_line(mech))
+        toc_lines.append("</details>")
+    toc_lines.append("</details>")
+    toc = "\n".join(toc_lines)
+    return toc
+
+
+def generate_tocs():
+    all_toc = generate_all_mechs_toc()
+    corp_toc = generate_toc_by_corp()
+    source_toc = generate_toc_by_source()
+    author_toc = generate_toc_by_author()
+
+    full_text = "\n\n---\n\n".join([all_toc, corp_toc, source_toc, author_toc])
+    return full_text
+
+
+def generate_readme():
+    mech_readmes = []
+
+    toc = generate_tocs()
+
+    for mech in MECHS:
+        mech_readme = generate_mech_readme(mech)
+        mech_readmes.append(mech_readme)
+
+    readme = "\n\n-------\n".join([toc] + mech_readmes)
+    return readme
+
+
+if __name__ == "__main__":
+    readme = generate_readme()
+    Path("README.md").write_text(readme)
+    print("done")
---
+++ objects.py
@@ -0,0 +1,376 @@
+from dataclasses import dataclass
+
+
+def list_content(enum_class):
+    all_contents = [
+        item
+        for item in dir(enum_class)
+        if not callable(getattr(enum_class, item)) and not item.startswith("__")
+    ]
+    useful_contents = [getattr(enum_class, item) for item in all_contents]
+    sorted_contents = sorted(useful_contents, key=lambda item: str(item))
+    return sorted_contents
+
+
+class Corps:
+    GMS = "GMS"
+    IPSN = "IPSN"
+    SSC = "SSC"
+    HORUS = "HORUS"
+    HA = "HA"
+    MFECANE = "MFECANE"
+
+
+CORPS: list[str] = list_content(Corps)
+
+
+class Sources:
+    CORE = "Core Book"
+    WALLFLOWER = "No Room for a Wallflower"
+    LONG_RIM = "Long Rim"
+    KARRAKIN_TRADE_BARONIES = "Field Guide: The Karrakin Trade Baronies"
+    MFECANE = "Field Guide: Mfecane"
+
+
+SOURCES: list[str] = list_content(Sources)
+
+
+class Authors:
+    MASSIF = "Massif Press"
+    NHP_SHAKA = "NHP-SHAKA"
+
+
+AUTHORS: list[str] = list_content(Authors)
+
+
+@dataclass
+class Mech:
+    name: str
+    corp: str
+    source: str = Sources.CORE
+    author: str = Authors.MASSIF
+    image_path: str | list[str] = None
+    summary: list[str] = None
+
+    def __str__(self):
+        return self.name
+
+    def __hash__(self):
+        return hash(self.name)
+
+
+class Mechs:
+    EVEREST = Mech(
+        name="everest",
+        corp=Corps.GMS,
+        summary=["Your first summit", "Balanced for most situations", "Safe fallback"],
+    )
+    SAGARMATHA = Mech(
+        name="sagarmatha",
+        corp=Corps.GMS,
+        source=Sources.WALLFLOWER,
+        summary=["Fat Everest", "All-round defender"],
+    )
+    BLACKBEARD = Mech(
+        name="blackbeard",
+        corp=Corps.IPSN,
+        image_path="https://static.wikia.nocookie.net/lancer/images/a/a1/Blackbeard.jpg/revision/latest?cb=20200509100622",
+        summary=[
+            "Berserker pattern melee",
+            "Agile",
+            "Grappling",
+            "Teamkilling",
+            "Chain Axe",
+        ],
+    )
+    CALIBAN = Mech(
+        "caliban",
+        corp=Corps.IPSN,
+        source=Sources.LONG_RIM,
+        image_path="https://external-preview.redd.it/uiHVNTr51s5O7LdbPLLwI-BkRrvRmbPZx_XW4m2jH1U.jpg?auto=webp&s=51e7c6a99d745f9c9a5e213a848219cfc2682382",
+        summary=[
+            "Boarding and Asset Reclamation",
+            "Enormous Shotgun",
+            "Doomguy",
+            "Knockback",
+        ],
+    )
+    DRAKE = Mech(
+        name="drake",
+        corp=Corps.IPSN,
+        image_path="https://static.wikia.nocookie.net/lancer/images/6/62/Drake.jpg/revision/latest?cb=20200509100716",
+        summary=["Heavy Assault/Fire Support", "Stationary bunker turret", "minigun"],
+    )
+    LANCASTER = Mech(
+        name="lancaster",
+        corp=Corps.IPSN,
+        image_path="https://static.wikia.nocookie.net/lancer/images/2/2c/Lancaster.jpg/revision/latest?cb=20200509101034",
+        summary=[
+            "Field Support/Repair",
+            "Space Horse - Battle Bus",
+            "The only healer",
+            "Neat utilities",
+            "Fireproof",
+        ],
+    )
+    KIDD = Mech(
+        name="kidd",
+        corp=Corps.IPSN,
+        source=Sources.WALLFLOWER,
+        image_path="https://pbs.twimg.com/media/EfE34JxXoAEWdnT.jpg:large",
+        summary=[
+            "Lancaster Variant",
+            "Frontline Rapid Support",
+            "Drones",
+            "Orbital Strike",
+        ],
+    )
+    NELSON = Mech(
+        name="nelson",
+        corp=Corps.IPSN,
+        image_path="https://pbs.twimg.com/media/D0da5CUWwAAwpO0.jpg:large",
+        summary=["Rapid Assault", "Fast Knight with a Spear", "Hit and Run"],
+    )
+    RALEIGH = Mech(
+        name="raleigh",
+        corp=Corps.IPSN,
+        image_path="https://static.wikia.nocookie.net/lancer/images/6/69/Raleigh.jpg/revision/latest?cb=20200509101441",
+        summary=[
+            "Frontline Assault",
+            "Reloading Gun Platform",
+            "Too many guns",
+            "Cowboy",
+            "Chest Cannon",
+        ],
+    )
+    TORTUGA = Mech(
+        name="tortuga",
+        corp=Corps.IPSN,
+        image_path="https://static.wikia.nocookie.net/lancer/images/0/03/Tortuga.jpg/revision/latest?cb=20200509101655",
+        summary=[
+            "Overwatch/Zone Defense",
+            "Ship defense and boarding",
+            "Auto-Shotgun",
+            "Controls close range",
+            "Smash through walls",
+        ],
+    )
+    VLAD = Mech(
+        name="vlad",
+        corp=Corps.IPSN,
+        image_path="https://static.wikia.nocookie.net/lancer/images/d/d1/Vlad.jpg/revision/latest?cb=20200509101717",
+        summary=[
+            "Nail people to the ground",
+            "Damage attackers because covered in spikes",
+            "Massive drill",
+        ],
+    )
+    ZHENG = Mech(
+        name="zheng",
+        corp=Corps.IPSN,
+        source=Sources.LONG_RIM,
+        image_path="https://pbs.twimg.com/media/D4xiHSrW4AA7IM5.jpg:large",
+        summary=[
+            "Unarmed Melee",
+            "Terrain destruction",
+            "Use the environment as a weapon",
+            "Throw people like a bowling ball",
+        ],
+    )
+
+    ATLAS = Mech(
+        name="atlas",
+        corp=Corps.SSC,
+        source=Sources.LONG_RIM,
+        image_path=[
+            "https://pbs.twimg.com/media/D55kqK-WwAAMyND?format=jpg&name=large",
+            "https://pbs.twimg.com/media/EMXOad_WwAMMkZM?format=jpg&name=large",
+        ],
+        summary=[
+            "A powerful, small-scale hunter that draws on the profiles of past pilots to power current action.",
+            "Attack on Titan",
+        ],
+    )
+    BLACK_WITCH = Mech(
+        name="black_witch",
+        corp=Corps.SSC,
+        image_path="https://static.wikia.nocookie.net/lancer/images/4/47/Blackwitch1.jpeg/revision/latest?cb=20210510124208",
+    )
+    DUSK_WING = Mech(
+        name="dusk_wing",
+        corp=Corps.SSC,
+        image_path="https://pbs.twimg.com/media/D28PCW3WsAIGZuU.jpg:large",
+    )
+    MONARCH = Mech(
+        name="monarch",
+        corp=Corps.SSC,
+        image_path="https://pbs.twimg.com/media/D0SvXINX4AEcN1Z.jpg:large",
+    )
+    DEATHS_HEAD = Mech(
+        name="death's head",
+        corp=Corps.SSC,
+        image_path="https://pbs.twimg.com/media/D3AyF5pXgAIM3t_.jpg:large",
+    )
+    SWALLOWTAIL = Mech(
+        name="swallowtail",
+        corp=Corps.SSC,
+        image_path="https://external-preview.redd.it/4ts71UdtaOTzZJOYApRc6U-m5G_CgzyTx9PGVqAKvn8.jpg?auto=webp&s=f54bcc86818f54f4e3008ceee2c50842201e93fa",
+    )
+    SWALLOWTAIL_RANGER = Mech(
+        name="swallowtail_ranger",
+        corp=Corps.SSC,
+        image_path="https://cdnb.artstation.com/p/assets/images/images/035/138/645/large/theotime-gm-ranger-variant-web.jpg?1614191125",
+    )
+    METALMARK = Mech(
+        name="metalmark",
+        corp=Corps.SSC,
+        image_path="https://static.wikia.nocookie.net/lancer/images/b/ba/Metalmark.jpeg/revision/latest?cb=20210510130854",
+    )
+    MOURNING_CLOAK = Mech(
+        name="mourning_cloak",
+        corp=Corps.SSC,
+        image_path="https://static.wikia.nocookie.net/lancer/images/1/15/MourningCloak.jpg/revision/latest?cb=20200509101339",
+    )
+    WHITE_WITCH = Mech(
+        name="white_witch",
+        corp=Corps.SSC,
+        source=Sources.KARRAKIN_TRADE_BARONIES,
+        image_path="https://pbs.twimg.com/media/EM6PMpPW4AAn668.jpg:large",
+    )
+    EMPEROR = Mech(
+        name="emperor",
+        corp=Corps.SSC,
+        source=Sources.KARRAKIN_TRADE_BARONIES,
+        image_path="https://external-preview.redd.it/YVWXiFNpCBcuyvcvPzX3Y1tjOnEQ_2XX2e2W1kueA0Y.jpg?auto=webp&s=f71b0a8c1ccb329dd0b0523260c8c18c88ec97a8",
+    )
+    ORCHIS = Mech(
+        name="orchis",
+        corp=Corps.SSC,
+        source=Sources.KARRAKIN_TRADE_BARONIES,
+        image_path="https://preview.redd.it/7ulbcgdnafn81.jpg?auto=webp&s=2af07f9391443e756c07e3f94108e0c7ca94f965",
+    )
+
+    BALOR = Mech(
+        name="balor",
+        corp=Corps.HORUS,
+        image_path="https://external-preview.redd.it/NfhQ53rmZpR7nZs7Giymy0QVlKnV_3KMS4hmuXEsIRU.jpg?auto=webp&s=6f5af6c3cb517bfa26be2535c89ab9b540a49043",
+    )
+    PEGASUS = Mech(
+        name="pegasus",
+        corp=Corps.HORUS,
+        image_path="https://pbs.twimg.com/media/Dm7Lwc1XsAElCJj.jpg:large",
+    )
+    LICH = Mech(
+        name="lich",
+        corp=Corps.HORUS,
+        source=Sources.LONG_RIM,
+        image_path="https://pbs.twimg.com/media/D6dRYIeW0AEhBg2.jpg:large",
+    )
+    MANTICORE = Mech(
+        name="manticore",
+        corp=Corps.HORUS,
+        image_path="https://pbs.twimg.com/media/D15ojOZW0AAcdQn.jpg:large",
+    )
+    HYDRA = Mech(
+        name="hydra",
+        corp=Corps.HORUS,
+        image_path="https://pbs.twimg.com/media/D3ZsrznWkAEDfHl.jpg:large",
+    )
+    GOBLIN = Mech(
+        name="goblin",
+        corp=Corps.HORUS,
+        image_path="https://static.wikia.nocookie.net/lancer/images/e/ef/Goblin.jpg/revision/latest?cb=20200509100851",
+    )
+    GORGON = Mech(
+        name="gorgon",
+        corp=Corps.HORUS,
+        image_path="https://pbs.twimg.com/media/DhCxXZtWsAEa0HH.jpg",
+    )
+    MINOTAUR = Mech(
+        name="minotaur",
+        corp=Corps.HORUS,
+        image_path="https://pbs.twimg.com/media/D0NqbpiXgAE6WLs.jpg:large",
+    )
+    KOBOLD = Mech(
+        name="kobold",
+        corp=Corps.HORUS,
+        source=Sources.LONG_RIM,
+        image_path="https://pbs.twimg.com/media/D4M6nGAWkAEmIQB.jpg:large",
+    )
+    CALENDULA = Mech(
+        name="calendula",
+        corp=Corps.HORUS,
+        source=Sources.KARRAKIN_TRADE_BARONIES,
+        image_path="https://pbs.twimg.com/media/FNXBM8JXIAsBmrc.jpg:large",
+    )
+
+    BARBAROSSA = Mech(
+        name="barbarossa",
+        corp=Corps.HA,
+        image_path="https://pbs.twimg.com/media/Ds471y1WsAAjpwJ.jpg",
+    )
+    ENKIDU = Mech(
+        name="enkidu",
+        corp=Corps.HA,
+        source=Sources.WALLFLOWER,
+        image_path="https://preview.redd.it/5ev5xcyprab81.jpg?auto=webp&s=768fba2788fc4d9c61d3bce3ffe1e8b32aa12905",
+    )
+    GENGHIS = Mech(
+        name="genghis",
+        corp=Corps.HA,
+        image_path="https://external-preview.redd.it/HnEsHvovvilWcBUdooCiFLgd99EzEMlwjMgzHIihSZA.jpg?auto=webp&s=10a5adb10e31c2fcba4936dec7b2b39731ec14b0",
+    )
+    GENGHIS_WORLDKILLER = Mech(
+        name="genghis worldkiller",
+        corp=Corps.HA,
+        source=Sources.WALLFLOWER,
+        image_path="https://cdn-animation.artstation.com/p/thumbnails/000/265/518/thumb.jpg",
+    )
+    ISKANDER = Mech(
+        name="iskander",
+        corp=Corps.HA,
+        image_path="https://pbs.twimg.com/media/D3V3iiaXkAEO29u.jpg:large",
+    )
+    NAPOLEON = Mech(
+        name="napoleon",
+        corp=Corps.HA,
+        image_path="https://external-preview.redd.it/flbCTCmqhnLOoWTzF6lRtYZPgpKtf9duWoLOqF6Zxiw.jpg?auto=webp&s=d968822a1fffa257adc347b4f59eadcdf1699ffe",
+    )
+    SALADIN = Mech(
+        name="saladin",
+        corp=Corps.HA,
+        image_path="https://pbs.twimg.com/media/D3HXdK3XcAEkLxk.jpg:large",
+    )
+    SHERMAN = Mech(
+        name="sherman",
+        corp=Corps.HA,
+        image_path=[
+            "https://pbs.twimg.com/media/D3w3yZ-XoAAGo_x?format=jpg&name=large",
+            "https://pbs.twimg.com/media/Df3RCD7XcAAws0N.jpg:large",
+        ],
+    )
+    TOKUGAWA = Mech(
+        name="tokugawa",
+        corp=Corps.HA,
+        image_path="https://64.media.tumblr.com/8642aa32ea7e7fde660a6d5daa833a8e/tumblr_pgedezbTgB1qlxan5o1_1280.jpg",
+    )
+    SUNZI = Mech(
+        name="sunzi",
+        corp=Corps.HA,
+        source=Sources.LONG_RIM,
+        image_path="https://ksr-ugc.imgix.net/assets/024/944/488/16ffd6f132e4ecd32ee139293b129442_original.jpg?ixlib=rb-4.0.2&w=700&fit=max&v=1556548469&gif-q=50&q=92&s=9f83682094aafa03d5a39cb69e4760e0",
+    )
+
+    IBUTHO = Mech(
+        name="ibutho",
+        corp=Corps.MFECANE,
+        source=Sources.MFECANE,
+        author=Authors.NHP_SHAKA,
+        image_path=[
+            "https://preview.redd.it/dgnj8y0n51q61.png?width=512&format=png&auto=webp&s=db1d0ba19fa81acd625a801898c1a4f48fab279d",
+            "https://preview.redd.it/fga3361n51q61.png?width=512&format=png&auto=webp&s=f25b79691edd1b70dabf504f33612ff2cfe1d754",
+        ],
+    )
+
+
+MECHS: list[Mech] = list_content(Mechs)
